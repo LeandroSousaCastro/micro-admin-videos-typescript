aa6f501e741b0a7cbd3e53cc957e72043ed501af
LeandroSousaCastro <leandro.desousacastro@gmail.com>
2022-05-07 13:52:16 -0300

    Instalando nestjs e criando namespaces para modularizar os sofwares, criando pacotes com pastas específicas para cada um deles

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  projects: [
+    '<rootDir>/src/@core',
+    '<rootDir>/src/nestjs'
+  ]
+};
