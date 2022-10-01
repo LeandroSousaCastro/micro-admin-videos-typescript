32425da95b798d0528c9ae28995489360d6b9ed4
LeandroSousaCastro <leandro.desouascastro@gmail.com>
2022-10-01 17:33:48 +0000

    test(nest): :white_check_mark: implementa teste de integração de criaação de categorias.

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# micro-admin-videos-typescript
+# Micro serviço de administração de catálogos de vídeos
 
-# Montagem do ambiente de desenvolvimento (Docker e IDE)
-# Criar uma aplicação TypeScript
-# Criar entidade de Categoria
-# Criar testes
-# Criar Casos de Uso e Repositório
-# Criar testes
+# Configurações
 
-# Repetir para as outras entidades Genre, Cast member Video
- 
-# Nest.js - Criação de API Rest
-# Integração com RabbitMQ e Encoder de vídeo
-# Testes E2E (End-to-End)
-
-src/
-   project1/
-         package.json
-   project2
-         package.json
+- copie o arquivo .env.exemplo e substitua as configuração de acordo com a necessidade de seu projeto.
--- src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
+++ src/nestjs/src/categories/__tests__/integration/categories.controller.int-spec.ts
@@ -3,10 +3,19 @@ import { CategoriesController } from '../../categories.controller';
 import { CategoriesModule } from '../../categories.module';
 import { DatabaseModule } from '../../../database/database.module';
 import { ConfigModule } from '../../../config/config.module';
-import { CreateCategoryUseCase } from '@fc/micro-videos/category/application';
+import {
+  CreateCategoryUseCase,
+  UpdateCategoryUseCase,
+  ListCategoriesUseCase,
+  GetCategoryUseCase,
+  DeleteCategoryUseCase,
+} from '@fc/micro-videos/category/application';
+import { CategoryRepository } from '@fc/micro-videos/category/domain';
+import { CATEGORY_PROVIDERS } from '../../category.providers';
 
 describe('CategoriesController Integration Tests', () => {
   let controller: CategoriesController;
+  let repository: CategoryRepository.Repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,13 +23,95 @@ describe('CategoriesController Integration Tests', () => {
     }).compile();
 
     controller = module.get(CategoriesController);
+    repository = module.get(
+      CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+    );
   });
 
   it('should controller be defined', () => {
     expect(controller).toBeDefined();
-    console.log(controller);
     expect(controller['createUseCase']).toBeInstanceOf(
       CreateCategoryUseCase.UseCase,
+    );
+    expect(controller['updateUseCase']).toBeInstanceOf(
+      UpdateCategoryUseCase.UseCase,
+    );
+    expect(controller['listUseCase']).toBeInstanceOf(
+      ListCategoriesUseCase.UseCase,
+    );
+    expect(controller['getUseCase']).toBeInstanceOf(GetCategoryUseCase.UseCase);
+    expect(controller['deleteUseCase']).toBeInstanceOf(
+      DeleteCategoryUseCase.UseCase,
+    );
+  });
+
+  describe('should create a category', () => {
+    const arrange = [
+      {
+        request: {
+          name: 'Test',
+        },
+        expectOutput: {
+          name: 'Test',
+          description: null,
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Test',
+          description: 'Test description',
+        },
+        expectOutput: {
+          name: 'Test',
+          description: 'Test description',
+          is_active: true,
+        },
+      },
+      {
+        request: {
+          name: 'Test',
+          description: 'Test description',
+          is_active: false,
+        },
+        expectOutput: {
+          name: 'Test',
+          description: 'Test description',
+          is_active: false,
+        },
+      },
+      {
+        request: {
+          name: 'Test',
+          is_active: false,
+        },
+        expectOutput: {
+          name: 'Test',
+          description: null,
+          is_active: false,
+        },
+      },
+    ];
+
+    test.each(arrange)(
+      'with request $request',
+      async ({ request, expectOutput }) => {
+        const output = await controller.create(request);
+        const entity = await repository.findById(output.id);
+        expect(entity).toMatchObject({
+          id: output.id,
+          name: expectOutput.name,
+          description: expectOutput.description,
+          is_active: expectOutput.is_active,
+          created_at: output.created_at,
+        });
+
+        expect(output.id).toBe(entity.id);
+        expect(output.name).toBe(expectOutput.name);
+        expect(output.description).toBe(expectOutput.description);
+        expect(output.is_active).toBe(expectOutput.is_active);
+        expect(output.created_at).toStrictEqual(entity.created_at);
+      },
     );
   });
 });
