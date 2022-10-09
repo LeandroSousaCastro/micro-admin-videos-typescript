06b424eead357dbf656f0b59f84c445e5dee051d
LeandroSousaCastro <leandro.desouascastro@gmail.com>
2022-10-09 13:28:57 +0000

    refactor(nest): :recycle: refatora o código do presenter meta para retornar como meta no corpo da mensagem de retorno e não mais paginationPresenter

--- src/nestjs/src/@share/presenters/collection.presenter.ts
+++ src/nestjs/src/@share/presenters/collection.presenter.ts
@@ -6,15 +6,15 @@ import {
 
 export abstract class CollectionPresenter {
   @Exclude()
-  protected paginationPresenter: PaginationPresenter;
+  protected meta: PaginationPresenter;
 
   constructor(props: PaginationPresenterProps) {
-    this.paginationPresenter = new PaginationPresenter(props);
+    this.meta = new PaginationPresenter(props);
   }
 
   @Expose({ name: 'meta' })
-  get meta() {
-    return this.paginationPresenter;
+  get _meta() {
+    return this.meta;
   }
 
   abstract get data();
